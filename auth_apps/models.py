843626a31e9a3bbe0af14ac5932054866e344d41
User <asqarservis0000@gmail.com>
2025-06-13 16:39:42 +0300

    updated

--- auth_apps/models.py
+++ auth_apps/models.py
@@ -1,3 +1,51 @@
-from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import UserManager, AbstractUser
+from django.db.models import Model, EmailField, CharField, TextChoices, DateTimeField, FileField
+from django.db.models.fields import BooleanField, TextField
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+
+    def _create_user(self, phone, password, **extra_fields):
+        if not phone:
+            raise ValueError("The given phone must be set")
+
+        user = self.model(phone=phone, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(phone, password, **extra_fields)
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(phone, password, **extra_fields)
+
+
+class User(AbstractUser):
+    class RoleType(TextChoices):
+        CHEF = 'chef', "Chef"
+        EMPLOYEE = 'employee', "Employee"
+        ADMIN = 'admin', "Admin"
+        DELIVERY = 'delivery', 'Delivery'
+
+    username = None
+
+    phone = CharField(max_length=20, unique=True)
+    password = CharField(max_length=128, null=True, blank=True)
+    role = CharField(max_length=30, choices=RoleType, default=RoleType.EMPLOYEE)
+    office_address = TextField(null=True, blank=True)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
