51570c02e79d29a16243ce35ab2a4225d0fa40df
User <asqarservis0000@gmail.com>
2025-06-16 11:21:37 +0300

    updated

--- apps/urls/assignments.py
+++ apps/urls/assignments.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.views import AssignmentCreateView, StudentAssignmentsListAPIView
 
 urlpatterns = [
-    path('save/assignments', AssignmentCreateView.as_view(), name='assignment-save'),
-    path('assignments/student/', StudentAssignmentsListAPIView.as_view(), name='student-assigment'),
+    path('save/assignments/', AssignmentCreateView.as_view(), name='assignment-save'),
+    path('assignments/student/', StudentAssignmentsListAPIView.as_view(), name='student-assigment')
 ]
