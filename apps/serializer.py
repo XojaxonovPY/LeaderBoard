05e4ad26b5b222e5710551b1d9a5902d3d45c773
User <asqarservis0000@gmail.com>
2025-06-17 17:08:24 +0300

    updated

--- apps/serializer.py
+++ apps/serializer.py
@@ -1,13 +1,19 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
-from apps.models import  Submission
-
-
+from apps.models import Submission, Homework
 
 
 class SubmissionModelSerialize(ModelSerializer):
     class Meta:
         model = Submission
-        fields = ('student', )
+        fields = ('student',)
+
+
+class HomeworkModelSerializer(ModelSerializer):
+    class Meta:
+        model = Homework
+        fields = ('id','title', 'description', 'points', 'start_date', 'deadline', 'line_limit', 'teacher', 'group',
+                  'file_extensions', 'ai_grading_prompt','created_at')
+        read_only_fields = ('id','created_at','teacher')
 
---
+++ apps/views/teachers.py
@@ -0,0 +1,13 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework.viewsets import ModelViewSet
+
+from apps.models import Homework
+from apps.permissions import IsTeacher
+from apps.serializer import HomeworkModelSerializer
+
+
+@extend_schema(tags=['teachers-homework'])
+class TeacherModelViewSet(ModelViewSet):
+    queryset = Homework.objects.all()
+    serializer_class = HomeworkModelSerializer
+    permission_classes = [IsTeacher]
