e889cda41ea617efae18682bc1a3130824f3b9af
User <asqarservis0000@gmail.com>
2025-06-18 14:57:50 +0300

    updated

--- root/settings.py
+++ root/settings.py
@@ -1,6 +1,10 @@
 from datetime import timedelta
+from os import getenv
 from os.path import join
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # =============================================================unfoald-admin
 # =-=-=-=-=-=-=-=-=-=-=-=-=-= Unfold CONFIG =-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -68,8 +72,12 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': getenv('DB_DJANGO'),
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'HOST': getenv('DB_HOST'),
+        'PORT': getenv('DB_PORT'),
     }
 }
 
