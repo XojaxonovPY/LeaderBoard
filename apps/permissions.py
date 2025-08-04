d390d1ce174dfb9269e352a83bea15e3e034799a
User <asqarservis0000@gmail.com>
2025-08-04 15:14:04 +0300

    updated

--- apps/permissions.py
+++ apps/permissions.py
@@ -4,5 +4,7 @@ from auth_apps.models import User
 
 
 class IsTeacher(BasePermission):
+    message = 'You are not a teacher'
+
     def has_permission(self, request, view):
         return bool(request.user and request.user.role == User.RoleType.Teacher)
